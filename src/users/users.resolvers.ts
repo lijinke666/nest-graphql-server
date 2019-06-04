2499ab2c0a30a074d2bebc459d0613feb3aad77a
Jinke.Li <1359518268@qq.com>
2019-06-04 15:49:02 +0800

    feat: add find one user query

--- src/users/users.resolvers.ts
+++ src/users/users.resolvers.ts
@@ -1,12 +1,19 @@
-import { ParseIntPipe, UseGuards } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { UsersService } from './users.service';
+import { User } from '../graphql.schema';
 
 @Resolver('Users')
 export class UsersResolvers {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
+
   @Query()
-  async getUsers() {
+  async getUsers(): Promise<User[]> {
     return await this.usersService.findAll();
+  }
+
+  @Query('user')
+  async getUserById(@Args('id', ParseIntPipe) id: number): Promise<User> {
+    return await this.usersService.findOneById(id);
   }
 }
